f043b36b31e0a2496d6efab41fa0909be7f48d20
Arick Hasan <hasanarick000@gmail.com>
2022-03-26 00:10:57 +0600

    structure 50% done

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>How react works?</h3>
+      <h3>Difference between props and state?</h3>
+      <h3>How useState works?</h3>
+    </div>
+  );
+};
+
+export default Footer;
